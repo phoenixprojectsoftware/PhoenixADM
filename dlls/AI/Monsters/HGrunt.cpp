c3b9fcd9b8f4f12e7a00180eefbef4457c1bf85e
sabianroberts <djdise@outlook.com>
2021-01-22 16:08:44 +0000

    Revert "Attempt to make duplicate of HGrunt "Combat Scientist""
    This reverts commit 75aebf6826b0a282e4b2e0007e5448997e4db1a0.

--- dlls/AI/Monsters/HGrunt.cpp
+++ dlls/AI/Monsters/HGrunt.cpp
@@ -42,12 +42,11 @@
 #include	"effects.h"
 #include	"customentity.h"
 #include	"Env/CBeam.h"
-#include "AI/Monsters/MilitaryWeapons.h"
 
 int g_fGruntQuestion;				// true if an idle grunt asked a question. Cleared when someone answers.
 
 extern DLL_GLOBAL int		g_iSkillLevel;
-/*
+
 //=========================================================
 // monster-specific DEFINE's
 //=========================================================
@@ -74,11 +73,53 @@ extern DLL_GLOBAL int		g_iSkillLevel;
 #define GUN_MP5						0
 #define GUN_SHOTGUN					1
 #define GUN_NONE					2
-*/
 
+//=========================================================
+// Monster's Anim Events Go Here
+//=========================================================
+#define		HGRUNT_AE_RELOAD		( 2 )
+#define		HGRUNT_AE_KICK			( 3 )
+#define		HGRUNT_AE_BURST1		( 4 )
+#define		HGRUNT_AE_BURST2		( 5 ) 
+#define		HGRUNT_AE_BURST3		( 6 ) 
+#define		HGRUNT_AE_GREN_TOSS		( 7 )
+#define		HGRUNT_AE_GREN_LAUNCH	( 8 )
+#define		HGRUNT_AE_GREN_DROP		( 9 )
+#define		HGRUNT_AE_CAUGHT_ENEMY	( 10) // grunt established sight with an enemy (player only) that had previously eluded the squad.
+#define		HGRUNT_AE_DROP_GUN		( 11) // grunt (probably dead) is dropping his mp5.
 
+//=========================================================
+// monster-specific schedule types
+//=========================================================
+enum
+{
+	SCHED_GRUNT_SUPPRESS = LAST_COMMON_SCHEDULE + 1,
+	SCHED_GRUNT_ESTABLISH_LINE_OF_FIRE,// move to a location to set up an attack against the enemy. (usually when a friendly is in the way).
+	SCHED_GRUNT_COVER_AND_RELOAD,
+	SCHED_GRUNT_SWEEP,
+	SCHED_GRUNT_FOUND_ENEMY,
+	SCHED_GRUNT_REPEL,
+	SCHED_GRUNT_REPEL_ATTACK,
+	SCHED_GRUNT_REPEL_LAND,
+	SCHED_GRUNT_WAIT_FACE_ENEMY,
+	SCHED_GRUNT_TAKECOVER_FAILED,// special schedule type that forces analysis of conditions and picks the best possible schedule to recover from this type of failure.
+	SCHED_GRUNT_ELOF_FAIL,
+};
 
+//=========================================================
+// monster-specific tasks
+//=========================================================
+enum 
+{
+	TASK_GRUNT_FACE_TOSS_DIR = LAST_COMMON_TASK + 1,
+	TASK_GRUNT_SPEAK_SENTENCE,
+	TASK_GRUNT_CHECK_FIRE,
+};
 
+//=========================================================
+// monster-specific conditions
+//=========================================================
+#define bits_COND_GRUNT_NOFIRE	( bits_COND_SPECIAL1 )
 
 class CHGrunt : public CSquadMonster
 {
