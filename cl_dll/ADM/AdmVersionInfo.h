60cb796ad91a0f6aa85ba261d50b65b1e8902fe4
Sabian Roberts <31491602+sabianroberts@users.noreply.github.com>
2022-06-06 00:28:01 +0100

    update version

--- cl_dll/ADM/AdmVersionInfo.h
+++ cl_dll/ADM/AdmVersionInfo.h
@@ -1,6 +1,6 @@
-#define clientBuild "2201-rev1"
+#define clientBuild "2206-rev1"
 #define Date __DATE__
-#define admVersion "2207"
+#define admVersion "2206"
 #define admAuthor "Admer456"
 #define admModifier "The Phoenix Project Software"
 #define admRepo "https://github.com/phoenixprojectsoftware/PhoenixADM"
